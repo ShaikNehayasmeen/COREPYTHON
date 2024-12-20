a04ec7c2c20a2b5659fcf2ee7c2f45316e814e19
ShaikNehayasmeen <167057841+ShaikNehayasmeen@users.noreply.github.com>
2024-12-20 21:49:11 +0530

    Delete modules_problems directory

--- modules_problems/Topic6Tuple.py
+++
@@ -1,6 +0,0 @@
-
-sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-
-sorted_list = sorted(sample_list, key=lambda x: x[-1])
-
-print("Sorted List:", sorted_list)
--- modules_problems/Topic9ConditionalCaptilize.py
+++
@@ -1,9 +0,0 @@
-def preLetterCase(string, letter):
-	index = string.find(letter)
-	if index != -1:
-		return string[:index].lower() + string[index:].upper()
-		return string
-print(preLetterCase("CAtCHa", "a"))  # Output: "cATCHA"
-print(preLetterCase("Preteen", "e"))  # Output: "prETEEN"
-print(preLetterCase("You've got this", "m"))  # Output: "you've got this"
-print(preLetterCase("Keep trying", "k"))  # Output: "KEEP TRYING"
